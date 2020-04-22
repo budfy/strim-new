bdec211ec67df828fe13ef4e55dd1629d4141ca5
budfy <serdyuk.ro@gmail.com>
2020-04-22 11:45:43 +0300

    Fullscreen ready

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 /** @format */
 
-// function errorHandler() {
-// 	alert("mozfullscreenerror");
-// }
-// document.documentElement.addEventListener(
-// 	"mozfullscreenerror",
-// 	errorHandler,
-// 	false,
-// );
+function errorHandler() {
+	alert("mozfullscreenerror");
+}
+document.documentElement.addEventListener(
+	"mozfullscreenerror",
+	errorHandler,
+	false,
+);
 
 // toggle full screen
-// function toggleFullScreen() {
-// 	if (
-// 		!document.fullscreenElement && // alternative standard method
-// 		!document.mozFullScreenElement &&
-// 		!document.webkitFullscreenElement
-// 	) {
-// 		// current working methods
-// 		if (document.documentElement.requestFullscreen) {
-// 			document.documentElement.requestFullscreen();
-// 		} else if (document.documentElement.mozRequestFullScreen) {
-// 			document.documentElement.mozRequestFullScreen();
-// 		} else if (document.documentElement.webkitRequestFullscreen) {
-// 			document.documentElement.webkitRequestFullscreen(
-// 				Element.ALLOW_KEYBOARD_INPUT,
-// 			);
-// 		}
-// 	} else {
-// 		if (document.cancelFullScreen) {
-// 			document.cancelFullScreen();
-// 		} else if (document.mozCancelFullScreen) {
-// 			document.mozCancelFullScreen();
-// 		} else if (document.webkitCancelFullScreen) {
-// 			document.webkitCancelFullScreen();
-// 		}
-// 	}
-// }
+function toggleFullScreen() {
+	if (
+		!document.fullscreenElement && // alternative standard method
+		!document.mozFullScreenElement &&
+		!document.webkitFullscreenElement
+	) {
+		// current working methods
+		if (document.documentElement.requestFullscreen) {
+			document.documentElement.requestFullscreen();
+		} else if (document.documentElement.mozRequestFullScreen) {
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullscreen) {
+			document.documentElement.webkitRequestFullscreen(
+				Element.ALLOW_KEYBOARD_INPUT,
+			);
+		}
+	} else {
+		if (document.cancelFullScreen) {
+			document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+			document.webkitCancelFullScreen();
+		}
+	}
+}
 
 $(document).ready(function () {
-	// $(window).on("load", function () {
-	// 	$(".popup-overlay").fadeIn(250);
-	// });
+	$(window).on("load", function () {
+		$(".popup-overlay").fadeIn(250);
+	});
 
-	// $(".full-btn").click(function () {
-	// 	toggleFullScreen();
-	// 	$(".popup-overlay").fadeOut(500);
-	// });
+	$(".full-btn").click(function () {
+		toggleFullScreen();
+		$(".popup-overlay").fadeOut(500);
+	});
 
 	function calcSize() {
 		let heightStart = $(
@@ -404,9 +404,9 @@ $(document).ready(function () {
 			$(".volume-icon>i").removeClass();
 			$(".volume-icon>i").addClass("fas fa-volume-down");
 		}
-		if (vol = 0) {
+		if ((vol = 0)) {
 			$(".volume-icon>i").removeClass();
-			$(".volume-icon>i").addClass('fas fa-volume-mute');
+			$(".volume-icon>i").addClass("fas fa-volume-mute");
 		}
 	});
 
